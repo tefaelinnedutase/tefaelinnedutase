44c97b1b3e2268f239c6e2e8318033046bc980fa
tefaelinnedutase <tefaelinnedutase@gmail.com>
2026-08-20 09:49:19 +0700

    Update website and deployment workflow

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+﻿document.addEventListener('DOMContentLoaded', () => {
   const nav = document.querySelector('.nav');
   const revealItems = document.querySelectorAll('.reveal');
   const form = document.getElementById('contactForm');
